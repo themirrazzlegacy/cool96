0cad24ad43dcc6b50d75486d5628e997607e2f9b
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-04-29 11:00:26 -0700

    Update dragndrop.js
    Add DroppableImage

--- dragndrop.js
+++ dragndrop.js
@@ -97,3 +97,55 @@ class DroppableTextarea {
     return this.el
   }
 }
+
+class DroppableImage {
+  constructor(src) {
+    this.evtEmitListeners=[];
+    this.el=document.createElement('img');
+    this.el.src=src;
+    var el$=this.el;
+    this.el.type='text';
+    this.el.className="exp-file-view dropzone droppable-html-image";
+    this.el.associatedDropZone=this;
+    this.el.dzone={
+      data:"DropZone:/dropableImage/Image",
+      accepts:['shell-icon'],
+      ondrop:function(e,t){
+        var evt=new FileDropEvent();
+        evt.filePath=e.getAttribute('path')
+        el$.associatedDropZone.emit(
+          "drop",
+          [
+            evt
+          ]
+        )
+      }
+    }
+  }
+  emit(evt,args){
+    var lst=this.evtEmitListeners;
+    for(var i=0;i<lst.length;i++){
+      try{
+        if(lst[i].eventId=evt){
+          lst[i].trigger(...args);
+        }
+      }catch(e){null}
+    }
+  }
+  on(evt,func){
+    this.evtEmitListeners.push({
+      eventId:evt,
+      trigger:func
+    })
+  }
+  getElement() {
+    return this.el
+  }
+  setSource(url) {
+    this.el.src=url
+  }
+  async setSourceToFile(path) {
+    var blob=await w96.FS.toBlob(path);
+    this.setSource(URL.createObjectURL(blob));
+  }
+}
